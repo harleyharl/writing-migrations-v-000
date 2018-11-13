c684fa7caec7b74eb420d1026fb7d2e83e50ba15
Harley Rayner <harleymteden@gmail.com>
2018-11-13 05:22:21 +0000

    automatically backed up by learn

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,10 @@
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+
+  def change
+    add_column :students do |s|
+      s.integer :grade
+      s.birthdate :string
+    end
+  end
+
+end
